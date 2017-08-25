563cb9be8fa51dd577d09a64dce271a733cb796b
yan <1290920112@qq.com>
2017-08-25 10:48:55 +0800

    项目提交

---
+++ src/main/java/com/yan/frame/commonuse/MainApp.java
@@ -0,0 +1,18 @@
+package com.yan.frame.commonuse;
+
+
+/**
+ * A Camel Application
+ */
+public class MainApp {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     */
+    public static void main(String... args)   {
+
+        System.out.println(123123123);
+    }
+
+}
+
---
+++ src/main/java/com/yan/frame/commonuse/SimpleService.java
@@ -0,0 +1,23 @@
+package com.yan.frame.commonuse;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * <p>Title:SimpleService </p>
+ * <p>Description:</p>
+ * Created with IntelliJ IDEA.
+ * User: qxy
+ * Date: 2017/8/24
+ * Time: 18:04
+ */
+@Data
+@ToString
+public class SimpleService {
+
+    private String userName;
+    private String detail;
+    private int age;
+
+
+}
