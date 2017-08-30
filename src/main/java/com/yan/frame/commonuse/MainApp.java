4e69aebae51e6cd97478086cf4d3fee531190cac
yan <1290920112@qq.com>
2017-08-30 18:37:38 +0800

    测试修改

--- src/main/java/com/yan/frame/commonuse/MainApp.java
+++ src/main/java/com/yan/frame/commonuse/MainApp.java
@@ -1,6 +1,11 @@
 package com.yan.frame.commonuse;
 
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * A Camel Application
  */
@@ -9,9 +14,14 @@ public class MainApp {
     /**
      * A main() so we can easily run these routing rules in our IDE
      */
-    public static void main(String... args)   {
-
-        System.out.println(123123123);
+    public static void main(String... args) throws FileNotFoundException {
+        try (BufferedInputStream bfis = new BufferedInputStream(new FileInputStream("E:\\test.text"))){
+            System.out.println(123);
+            int a  = 1_000_000;
+            System.out.println(a);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
